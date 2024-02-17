b4b9e85f23c8f7cb0bbe746a5391075f8ef8aa72
Rain <therainy.dev@tutanota.de>
2024-02-17 20:26:16 +0000

    count predictions and total

--- arrowpredictor.c
+++ arrowpredictor.c
@@ -15,6 +15,7 @@ int main(void) {
 	keypad(stdscr, TRUE);
 	noecho();
 	int c;
+	uint64_t t = 0, p = 0;
 	/* data structure:
 	 * array of 256 uint64_t, indexed by uint8_t, left = 0, right = 1
 	 * uint8_t structure is ABCDEFGH, A-G are previous presses, H is press to predict
@@ -25,13 +26,20 @@ int main(void) {
 		fputs("memory allocation failed\n", stderr);
 		return 1;
 	}
-	printw("press left or right arrow key to be predicted, or press Q to quit\n");
+	printw(
+		"press left or right arrow key to be predicted, or press Q to quit\n"
+	);
 	while (1) {
 		prev <<= 1;
 		printw(
+			"total: %"PRIu64" presses\n"
+			"correctly predicted: %"PRIu64" presses\n"
 			"prev: %c%c%c%c%c%c%c\n"
 			"before: %"PRIu64" <\n"
-			"        %"PRIu64" >\n",
+			"        %"PRIu64" >\n"
+			"predicted: %c\n",
+			t,
+			p,
 			(prev & UINT8_C(128)) ? '>' : '<',
 			(prev & UINT8_C( 64)) ? '>' : '<',
 			(prev & UINT8_C( 32)) ? '>' : '<',
@@ -40,21 +48,29 @@ int main(void) {
 			(prev & UINT8_C(  4)) ? '>' : '<',
 			(prev & UINT8_C(  2)) ? '>' : '<',
 			seq[prev],
-			seq[prev|UINT8_C(1)]
+			seq[prev|UINT8_C(1)],
+			(seq[prev] > seq[prev|UINT8_C(1)]) ? '<': '>'
 		);
 		c = getch();
 		clear();
 		switch (c) {
 			case KEY_LEFT:
 				printw("you pressed left arrow\n");
+				if (seq[prev] > seq[prev|UINT8_C(1)]) {
+					p++;
+				}
 				break;
 			case KEY_RIGHT:
 				printw("you pressed right arrow\n");
+				if (seq[prev] <= seq[prev|UINT8_C(1)]) {
+					p++;
+				}
 				prev |= UINT8_C(1);
 				break;
 			case 'q':
 				cleanup(0);
 		}
 		seq[prev]++;
+		t++;
 	}
 }
