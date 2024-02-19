4e48393e8b93b43b9c6f8967f4034462c6536da7
Rain <therainy.dev@tutanota.de>
2024-02-19 01:33:49 +0000

    complete neuron structure, add prototypes for manipulating neurons

--- nn.h
+++ nn.h
@@ -1,11 +1,19 @@
 #ifndef ARROW_NN_H
 #define ARROW_NN_H
 
-float sigmoid(float x);
-
 struct neuron {
+	size_t num_ins;
+	struct neuron *ins;
+	float *in_weights;
+	float out;
 };
 
+float sigmoid(float x);
+void evaluate(struct neuron *neuron);
+
+struct neuron *alloc_layer(size_t size);
+
 void free_neuron(struct neuron *neuron);
+void free_layer(struct neuron *layer, size_t size);
 
 #endif
